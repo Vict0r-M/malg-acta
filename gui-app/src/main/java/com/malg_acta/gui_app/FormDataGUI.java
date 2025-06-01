361144faf4259ac4dcbb9ffbe4b60fa23af8308e
ioanarusti <rusti_ioana@yahoo.com>
2025-06-01 23:22:57 +0300

    GUI and CLI; log panel modified

--- gui-app/src/main/java/com/malg_acta/gui_app/FormDataGUI.java
+++ gui-app/src/main/java/com/malg_acta/gui_app/FormDataGUI.java
@@ -1,10 +1,13 @@
 package com.malg_acta.gui_app;
 
-public class FormData {
+public class FormDataGUI {
+	public String protocol;
 	public String probe_date;
 	public String beneficiar;
 	public String clasa_betonului;
 	public int numar_teste;
 	public String internal_code;
 	public String try_date;	
+	public String obiectiv;
+	public String element;
 }
