138c2ee5593e505856664a151437cef1d1b5ee40
ioanarusti <rusti_ioana@yahoo.com>
2025-06-01 14:00:19 +0300

    Implemented GUI

---
+++ gui-app/src/main/java/com/malg_acta/gui_app/FormData.java
@@ -0,0 +1,10 @@
+package com.malg_acta.gui_app;
+
+public class FormData {
+	public String probe_date;
+    public String beneficiar;
+    public String clasa_betonului;
+    public int numar_teste;
+    public String internal_code;
+    public String try_date;	
+}
