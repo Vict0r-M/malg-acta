442e5759bf9fefc60aab79766a629e5885719120
ioanarusti <rusti_ioana@yahoo.com>
2025-06-07 23:04:56 +0300

    Working version of GUI

--- gui-app/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.malg_acta.gui_app {
-    requires javafx.controls;
-	requires com.fasterxml.jackson.core;
-	requires com.fasterxml.jackson.databind;
-	requires javafx.base;
-    exports com.malg_acta.gui_app;
-    
-    opens com.malg_acta.gui_app to com.fasterxml.jackson.databind;
-}
